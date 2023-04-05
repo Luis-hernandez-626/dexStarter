c73c2724e4e22e501d6ea67e81361f3916f9c0f3
Luis20099 <racoon553@gmail.com>
2023-04-05 13:30:40 -0700

    4/5 update
    update

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import Eth from "../eth.svg";
+import Logo from "../moralis-logo.svg";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className= "leftH">
+        <img src={Logo} alt='logo for exchange' className="logo"/>
+
+        <Link to ="/" className='link'>
+          <div className="headerItem">Swap</div>
+        </Link>
+
+        <Link to ="/tokens" className='link'>
+          <div className="headerItem">Token</div>
+        </Link>
+
+
+      </div>
+      <div className="rigthH">
+        <div className ="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        
+        <div className="connectButton">
+          connect
+        </div>
+
+      </div>
+
+    </header>
   )
 }
 
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import{ Input, Popover, Radio, Modal, message } from "antd";
+import{
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 function Swap() {
   return (
-    <div>Swap</div>
+    <div className="tradebox">
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+      </div>
+    </div>
   )
 }
+import { Form } from 'react-router-dom'
 
 export default Swap
