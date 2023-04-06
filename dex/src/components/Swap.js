486a73f70904e145f9c3dc1f08464c88b1c36bc5
Luis20099 <racoon553@gmail.com>
2023-04-05 17:49:51 -0700

    not done yet
    4/5 5:49pm update

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,20 +1,86 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import{ Input, Popover, Radio, Modal, message } from "antd";
 import{
   ArrowDownOutlined,
   DownOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
+import tokenList from "../tokenList.json";
+
+
 
 function Swap() {
+const [slippage, setSlippage] = useState(2.5);
+const [tokenOneAmount, setTokenOneAmount] = useState(null);
+const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+const [tokenOne, setTokenOne]= useState(tokenList[0]);
+const [tokenTwo, setTokenTwo]= useState(tokenList[1]);
+
+
+
+
+
+function handelSlippageChange(e){
+  setSlippage(e.target.value);
+
+}
+
+function changeAmount(e){
+  setTokenOneAmount(e.target.value);
+}
+
+
+
+
+const settings = (
+  <>
+    <div>Slippaage Tolerance</div>
+    <div>
+      <Radio.Group value={slippage} onChange={handelSlippageChange}>
+        <Radio.Button value={0.5}>0.5%</Radio.Button> 
+        <Radio.Button value={2.5}>2.5%</Radio.Button> 
+        <Radio.Button value={5}>5%</Radio.Button> 
+      </Radio.Group>
+    </div>
+  
+  </>
+    
+
+)
+
+
   return (
-    <div className="tradebox">
+    <div className="tradeBox">
       <div className="tradeBoxHeader">
         <h4>Swap</h4>
+        <Popover 
+        
+          content = {settings}
+          title = "settings"
+          trigger="click"
+          placement="bottomRight">
+
+         <SettingOutlined className="cog" />
+         </Popover>
+      </div>
+      <div className="inputs">
+        <Input placeholder="0" value = {tokenOneAmount} onChange={changeAmount} />
+        <Input placeholder="0" value = {tokenTwoAmount} disabled = {true} />
+        <div className="assetOne">
+          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo"/>
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className="assetTwo">
+          <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo"/>
+            {tokenTwo.ticker}
+            <DownOutlined />
+
+        </div>
       </div>
     </div>
   )
 }
-import { Form } from 'react-router-dom'
+
 
 export default Swap
